0f3c043a63ebb49fbb5c4b24e2a2cc9c42732dfd
zhangshuai <zhangshuai08@sunlands.com>
2021-03-13 20:44:39 +0800

    feat: 修改useValue

--- client/src/hooks/common/useValues.ts
+++ client/src/hooks/common/useValues.ts
@@ -2,22 +2,22 @@
  * @description: 多参数的useState
  * @author: zs
  * @Date: 2020-08-25 16:09:31
- * @LastEditTime: 2020-08-29 16:31:09
+ * @LastEditTime: 2021-03-13 20:42:45
  * @LastEditors: zs
  */
-import { useState, useCallback } from 'react';
+ import { useState, useCallback } from 'react';
 
-function useValues <T>(initialValue) {
-  const [values, dispatch] = useState(initialValue)
-
-  const setDispatch = useCallback((_values: Partial<T>) => {
-    if (typeof _values !== 'object') {
-      return console.warn('values required type is object!');
-    }
-    return dispatch((values: T) => ({ ...values, ..._values }))
-  }, [dispatch])
-
-  return [values, setDispatch]
-}
-
-export default useValues ;
+ function useValues<T>(initialValue: T) {
+   const [values, dispatch] = useState(initialValue)
+ 
+   const setDispatch = useCallback((_values: Partial<T>) => {
+     if (typeof _values !== 'object') {
+       return console.warn('values required type is object!');
+     }
+     return dispatch((values: T) => ({ ...values, ..._values }))
+   }, [dispatch])
+ 
+   return [values, setDispatch] as [T, React.Dispatch<Partial<T>>]
+ }
+ 
+ export default useValues;
